62ba03c22eca8b9e81d3858294a1b78af7db05a6
InfinityJammyyy <143579594+InfinityJammyyy@users.noreply.github.com>
2024-12-17 16:37:39 -0800

    Update 11bash.sh
    Ooops

--- 11bash.sh
+++ 11bash.sh
@@ -6,7 +6,7 @@ if [[ $EUID -ne 0 ]]; then
     exit 1
 fi
 
-sudo apt update -y && sudo apt upgrade -y && sudo apt install -y dialog mawk pam-pwquality
+sudo apt update -y && sudo apt upgrade -y && sudo apt install -y dialog mawk
 
 # Function to create a backup of a file
 backup_file() {
