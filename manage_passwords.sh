3a38f15170b6476877aefdc309485e7adbf3a0e7
InfinityJammyyy <143579594+InfinityJammyyy@users.noreply.github.com>
2024-12-19 14:41:45 -0800

    Create manage_passwords.sh

---
+++ manage_passwords.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo 'Managing user passwords...'
+sudo passwd -l root
+backup_file /etc/login.defs
+sudo sed -i 's/PASS_MAX_DAYS.*$/PASS_MAX_DAYS 90/;s/PASS_MIN_DAYS.*$/PASS_MIN_DAYS 10/;s/PASS_WARN_AGE.*$/PASS_WARN_AGE 7/' /etc/login.defs
